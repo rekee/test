9660bc5ac0aa647aae47278102bd4000e43c5978
rekee <190307092@qq.com>
2019-12-07 19:06:07 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public void  main (String[] args) {
+        System.out.println("hello world");
+        ArrayList list = new ArrayList();
+
+    }
+
+}
